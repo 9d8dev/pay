2cbaff9caf9fda1e8ee93ac503f0256701843c2b
Bridger Tower <bridgertower@gmail.com>
2024-12-05 14:23:49 -0700

    wip

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -20,11 +20,11 @@ const font = Font({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={font.className}>
-      <body className="min-h-screen text-zinc-950 bg-zinc-50 selection:bg-orange-200">
-        <main className="p-6 sm:p-12 grid gap-12 max-w-7xl">
-          <Header />
+      <body className="min-h-screen">
+        <main>
+          {/* <Header /> */}
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </main>
       </body>
     </html>
@@ -35,10 +35,7 @@ const Header = () => {
   return (
     <nav className="sticky top-12 z-10">
       <h2 className="font-medium">
-        <Link href="/" className="group">
-          Design Books
-          <span className="hidden group-hover:inline-block"> *</span>
-        </Link>
+        <Link href="/" className="group"></Link>
       </h2>
     </nav>
   )
