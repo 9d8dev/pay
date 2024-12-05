ccad98febe08952180fcc16f3a3e570dceedce88
Bridger Tower <bridgertower@gmail.com>
2024-12-05 14:20:33 -0700

    wip

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,9 +1,14 @@
-export const revalidate = 600
+// export const revalidate = 600
 
 export default function Index() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="grid gap-8">
+      <h1>
+        💸 Pay -- Payload Starter by <a href="https://9d8.dev">9d8</a>
+      </h1>
+      <a className="underline underline-offset-2" href="https://github.com/9d8dev/pay">
+        Github: 9d8dev/pay
+      </a>
     </div>
   )
 }
