8b235454381aec738b0ab195a0cdf9da565fd550
Bridger Tower <bridgertower@gmail.com>
2024-12-05 15:13:01 -0700

    wip

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -6,6 +6,21 @@ export default function Index() {
       <h1>
         💸 Pay -- Payload Starter by <a href="https://9d8.dev">9d8</a>
       </h1>
+
+      {/* Delete this */}
+      <HowToUse />
     </div>
+  )
+}
+
+const HowToUse = () => {
+  return (
+    <section className="p-6 border rounded-md bg-accent/30">
+      <h2>9d8/pay is a PayloadCMS Starter</h2>
+      <p className="mt-6">How </p>
+      <ul>
+        <li></li>
+      </ul>
+    </section>
   )
 }
